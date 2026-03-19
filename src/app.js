235ef856ec86e41ec694452ce2e8a8a5a407d79e
Fabrizzio Rebuffo <fabrizziorebuffo12@gmail.com>
2026-03-19 03:39:49 +0000

    fin proyecto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  const suits = ["heart", "diamond", "spade", "club"];
+  const symbols = {
+    heart: "♥",
+    diamond: "♦",
+    spade: "♠",
+    club: "♣"
+  };
+
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  // Elegir aleatorios
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+
+  // HTML
+  let card = document.getElementById("card");
+  let top = document.getElementById("top");
+  let bottom = document.getElementById("bottom");
+  let number = document.getElementById("number");
+
+  // contenido
+  top.innerHTML = symbols[randomSuit];
+  bottom.innerHTML = symbols[randomSuit];
+  number.innerHTML = randomValue;
+
+  // clase de color
+  card.classList.add(randomSuit);
 };
